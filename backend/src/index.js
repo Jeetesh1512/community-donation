689cff7dfc5c3a273b6e8840eae73f28b4e5e310
Jeetesh1512 <abroljeetesh1512@gmail.com>
2025-04-30 01:25:07 +0530

    minor change

--- backend/src/index.js
+++ backend/src/index.js
@@ -23,7 +23,7 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://community-donation-frontend.vercel.app/",
     credentials: true
   }
 });
