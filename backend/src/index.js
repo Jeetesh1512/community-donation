352aef2c54013b981eadd0808344f82d3413a842
Jeetesh Abrol <abroljeetesh1512@gmail.com>
2025-06-15 17:00:55 +0530

    change in vercel file

--- backend/src/index.js
+++ backend/src/index.js
@@ -80,7 +80,7 @@ io.on("connection", (socket) => {
 });
 
 // Start server
-server.listen(8080, async () => {
+server.listen(process.env.PORT, async () => {
   await connect();
-  console.log("Server is running on port 8080");
+  console.log(`Server is running on port 8080 ${process.env.PORT}`);
 });
